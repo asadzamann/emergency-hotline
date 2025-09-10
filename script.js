5700c6c8be32a68cbd2204664ce4c26f3b7cb318
Asad Zaman <10asadzaman@gmail.com>
2025-09-11 03:54:57 +0600

    design finished

--- script.js
+++
@@ -1,8 +0,0 @@
-const callNational = document.getElementById('callNational').addEventListener('click', function(e){
-    console.log('National Clicked');
-})
-const callPolice = document.getElementById('callPolice').addEventListener('click', function(e){
-    selectedContainer.append;
-})
-
-const selectedContainer = document.getElementById('selectedContainer').innerHTML;
