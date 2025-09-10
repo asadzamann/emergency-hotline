bd5ec2ca23573c1d8aca72d7b900b1a7bb46e55e
Asad Zaman <10asadzaman@gmail.com>
2025-09-10 13:58:57 +0600

    checking

---
+++ script.js
@@ -0,0 +1,8 @@
+const callNational = document.getElementById('callNational').addEventListener('click', function(e){
+    console.log('National Clicked');
+})
+const callPolice = document.getElementById('callPolice').addEventListener('click', function(e){
+    selectedContainer.append;
+})
+
+const selectedContainer = document.getElementById('selectedContainer').innerHTML;
