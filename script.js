308d77dbdb1ad7332ed6d36a242eb34f038b45d7
Asad Zaman <10asadzaman@gmail.com>
2025-09-12 23:01:39 +0600

    added coin reduction feature

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-let count = 0;
-const heartButtons = document.getElementsByClassName('heartBtn');
-const heartValue = document.getElementsByClassName('heartValue');
+function heartCount() {
+    let count = 0;
+    const heartButtons = document.getElementsByClassName('heartBtn');
+    const heartValue = document.getElementsByClassName('heartValue');
 
-for (const button of heartButtons) {
-    button.addEventListener('click', function () {
-        count++;
-        for (let value of heartValue) {
-            value.innerText = count;
-        }
-    })
+    for (const button of heartButtons) {
+        button.addEventListener('click', function () {
+            count++;
+            for (let value of heartValue) {
+                value.innerText = count;
+            }
+        })
+    }
+
 }
 
-
-getAlert('callNational');
-getAlert('callPolice');
-getAlert('callFire');
-getAlert('callAmbulance');
-getAlert('callWoman');
-getAlert('callAnti');
-getAlert('callElectricity');
-getAlert('callBrac');
-getAlert('callRailway');
-
-function getAlert(id) {
+function showAlert(id) {
     const callBtn = document.getElementById(id);
     callBtn.addEventListener('click', function () {
         const parentCard = this.closest('.parent-card');
+
+        if (!parentCard) return;
+
         const serviceName = parentCard.querySelector('.service-name').innerText;
         const number = parentCard.querySelector('.number').innerText;
         const showText = 'Service name: ' + serviceName + '\n' + 'Service number: ' + number;
@@ -33,3 +28,50 @@ function getAlert(id) {
     })
 
 }
+
+
+function coinUse(id){
+     let count = 0;
+     let callCost = 20;
+    const callBtn = document.getElementById(id);
+    const coinValue = document.getElementsByClassName('coinValue');
+
+   callBtn.addEventListener('click', function () {
+            count++;
+            for (let value of coinValue) {
+                coinValueNumber = parseInt(value.innerText);
+                const newCoinValue = coinValueNumber - callCost;
+                if(newCoinValue < 20){
+                    alert('Insufficient Balance');
+                    return;
+                }
+                
+                value.innerText = newCoinValue;
+            }
+        })
+
+}
+
+coinUse('callNational');
+coinUse('callPolice');
+coinUse('callFire');
+coinUse('callAmbulance');
+coinUse('callWoman');
+coinUse('callAnti');
+coinUse('callElectricity');
+coinUse('callBrac');
+coinUse('callRailway');
+
+heartCount();
+
+showAlert('callNational');
+showAlert('callPolice');
+showAlert('callFire');
+showAlert('callAmbulance');
+showAlert('callWoman');
+showAlert('callAnti');
+showAlert('callElectricity');
+showAlert('callBrac');
+showAlert('callRailway');
+
+
